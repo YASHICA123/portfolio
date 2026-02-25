1e666baa7c7aadc8dd3767f4151a16f9d3e6bcd9
YASHICA123 <yashicamittal12@gmail.com>
2026-02-25 22:27:34 +0530

    Fix merge conflicts in config files

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/dev/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-import "./.next/dev/types/routes.d.ts";
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
->>>>>>> ee87ecdb9b93771c228ff125cd42f24976ff7f6c
